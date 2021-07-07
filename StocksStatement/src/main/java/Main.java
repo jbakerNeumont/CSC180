bb967c356e388f437cd5185a3307fc9af59cf173
Joseph Baker <jbaker@student.neumont.edu>
2021-07-07 09:51:07 -0600

    Day 1 commit of Stock Statement assignment.

---
+++ StocksStatement/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("I am alive!");
+
+	}
+	
+	public static void readJSON() {
+		try
+		{
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+}
