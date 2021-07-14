a68b8cdbc2f8fced0d8d812630bfca7c39b519a9
Joseph Baker <jbaker@student.neumont.edu>
2021-07-13 20:15:58 -0600

    All table rows are formatted and the math for total cash and stock value are completed. Just need HTML to PDF part.

--- StocksStatement/src/main/java/Main.java
+++ StocksStatement/src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 
 public class Main {
 
@@ -12,19 +13,41 @@ public class Main {
 		readJSON();
 	}
 	
+	public static void createPDF(String strFileName) {
+		try {
+			File outFile = new File(strFileName);
+			OutputStream os = new FileOutputStream(outFile);
+			PdfRendererBuilder builder = new PdfRendererBuilder();
+			
+			builder.toStream(os);
+			builder.run();
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+	
 	public static void writeHTML(JSONObject customerRecord) {
 		String strHTMLTop = "<!DOCTYPE html><html><body>\r\n";
         String strHTMLBottom = "</body></html>";
         String strHTMLCustInfo = String.format("<h3>%s</h3><p>%s %s</p>\r\n<p>%s</p><br>",
                 LocalDate.now(), customerRecord.get("first_name"), customerRecord.get("last_name"), customerRecord.get("ssn"));
-        String strHTMLTableTradeTop = "<table style=\"width:100%\">\r\n<tr><th>Type</th>"
+        String strHTMLTableTradeTop = "<table style=\"width:100%; border:2px solid black;\">\r\n<tr><th>Type</th>"
                 + "<th>Symbol</th><th>Price</th><th>Shares</th><th>Total</th></tr>";
         String strHTMLTableTradeBottom = "</table>\r\n";
-        String strHTMLSummary = String.format("<p>Cash Value: %s</p>\r\n<p>Stock Value:"
-                + " %s</p>\r\n", "cash", "stock");
+        String strHTMLSummary;
         String strFileName = customerRecord.get("account_number").toString();
         
 		FileWriter fw;
+		
+		JSONArray trades = (JSONArray) customerRecord.get("stock_trades");
+		double cashValue = Double.parseDouble(customerRecord.get("beginning_balance").toString().substring(1));
+		double stockValue = 0.00;
+		String trade_type;
+		String stock_symbol;
+		long count_shares;
+		String price_per_share;
+		double total_trade_value;
 		
 		try
 		{
@@ -32,8 +55,34 @@ public class Main {
 			fw.write(strHTMLTop);
 			fw.write(strHTMLCustInfo);
 			fw.write(strHTMLTableTradeTop);
-			//rows of trades
+			for(int x = 0; x < trades.size(); x++) 
+			{
+				JSONObject trade = (JSONObject) trades.get(x);
+				trade_type = (String) trade.get("type");
+				stock_symbol = (String) trade.get("stock_symbol");
+				count_shares = (long) trade.get("count_shares");
+				price_per_share = (String) trade.get("price_per_share");
+				total_trade_value = count_shares * Double.parseDouble(price_per_share.substring(1));
+				
+				if(trade_type.equals("Sell")) {
+					cashValue += total_trade_value;
+					stockValue -= total_trade_value;
+				} else if(trade_type.equals("Buy")) {
+					cashValue -= total_trade_value;
+					stockValue += total_trade_value;
+				}
+				
+				String strHTMLRowData = String.format("<tr style=\"text-align:center;\"><td style=\"border:1px double;\">%s</td>"
+						+ "<td style=\"border:1px double;\">%s</td>"
+						+ "<td style=\"border:1px double;\">%s</td>"
+						+ "<td style=\"border:1px double;\">%s</td>"
+						+ "<td style=\"border:1px double;\">%s</td></tr>",
+						trade_type, stock_symbol, price_per_share, count_shares, "$" + Math.round(total_trade_value * 100.0) / 100.0);
+				fw.write(strHTMLRowData);
+			}
 			fw.write(strHTMLTableTradeBottom);
+			strHTMLSummary = String.format("<p>Cash Value: %s</p>\r\n<p>Stock Value:"
+	                + " %s</p>\r\n", "$" + Math.round(cashValue * 100.0) / 100.0, "$" + Math.round(stockValue * 100.0) / 100.0);
 			fw.write(strHTMLSummary);
 			fw.write(strHTMLBottom);
 			fw.close();
@@ -57,18 +106,6 @@ public class Main {
 				JSONObject customerRecord = (JSONObject) jsonA.get(i);
 				writeHTML(customerRecord);
 				
-//				long accountNumber = (long) customerRecord.get("account_number");
-//				System.out.println(accountNumber);
-//				
-//				JSONArray trades = (JSONArray) customerRecord.get("stock_trades");
-//				for(int x = 0; x < trades.size(); x++) 
-//				{
-//					JSONObject trade = (JSONObject) trades.get(x);
-//					String trade_type = (String) trade.get("type");
-//					System.out.println(trade_type);
-//				}
-//				
-//				System.out.println();
 			}
 			
 			System.out.println("Succefully read from JSON...");
